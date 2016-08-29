8f9d1f1ea9ffd2f486faa3fe8a827b2b36a2c5b8
soulchainer <soulchainer@gmail.com>
2016-08-29 16:32:29 +0200

    Close Schema declaration in searchQuery.js

--- app/models/searchQuery.js
+++ app/models/searchQuery.js
@@ -15,6 +15,6 @@ var querySchema = new Schema({
     type: Date,
     required: true
   }
-}/*, { capped: 1048576})*/; /* Limit space to a max of 1 MB (in bytes). Commented because Mlab is blocking more than one capped collection in their Free service */
+}/*, { capped: 1048576}*/); /* Limit space to a max of 1 MB (in bytes). Commented because Mlab is blocking more than one capped collection in their Free service */
 
 module.exports = mongoose.model('SearchQuery', querySchema, 'searchQuery');
